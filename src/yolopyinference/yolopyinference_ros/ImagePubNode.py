671a7b2abc20b016f2aad5cdd3c8bb49914d54dc
lkk688 <kaikai.liu@sjsu.edu>
2023-03-13 18:10:28 +0000

    add image publish node

---
+++ src/yolopyinference/yolopyinference_ros/ImagePubNode.py
@@ -0,0 +1,103 @@
+# Import the necessary libraries
+import rclpy  # Python Client Library for ROS 2
+from rclpy.node import Node  # Handles the creation of nodes
+from sensor_msgs.msg import Image  # Image is the message type
+from cv_bridge import CvBridge  # Package to convert between ROS and OpenCV Images
+import cv2  # OpenCV library
+import argparse
+from datetime import datetime
+import os
+import yaml
+from natsort import natsorted #natsorted identifies numbers anywhere in a string and sorts them naturally
+#ref https://github.com/klintan/ros2_video_streamer/blob/master/camera_simulator/camera_simulator.py
+
+class ImagePublisher(Node):
+
+    """
+    Create an ImagePublisher class, which is a subclass of the Node class.
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Class constructor to set up the node
+        """
+        # Initiate the Node class's constructor and give it a name
+        super().__init__('image_publisher')
+
+        image_topic_ = self.declare_parameter("image_topic", "camera/color/image_raw").value
+        self.timer_period = self.declare_parameter("timer_period", 0.1).value
+        self.camera_name_ = self.declare_parameter("camera_name", "narrow_stereo").value
+
+        self.type = kwargs["type"]
+        self.loop = kwargs["loop"]
+
+        # Create the publisher. This publisher will publish an Image
+        # to the video_frames topic. The queue size is 10 messages.
+        self.publisher_ = self.create_publisher(Image, image_topic_, 10)
+
+        # Create the timer
+        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+
+        # Create a VideoCapture object
+        # The argument '0' gets the default webcam.
+        self.cap = cv2.VideoCapture(0)
+
+        # Used to convert between ROS and OpenCV images
+        self.br = CvBridge()
+
+    def timer_callback(self):
+        """
+        Callback function.
+        This function gets called every 0.1 seconds.
+        """
+        # Capture frame-by-frame
+        # This method returns True/False as well
+        # as the video frame.
+        ret, frame = self.cap.read()
+
+        if ret == True:
+        # Publish the image.
+        # The 'cv2_to_imgmsg' method converts an OpenCV
+        # image to a ROS 2 image message
+        self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
+
+        # Display the message on the console
+        self.get_logger().info('Publishing video frame')
+
+
+def main(args=None):
+
+    parser = argparse.ArgumentParser(description="Video file or files to load")
+    parser.add_argument("--path", type=str, default="",
+                        required=True, help="path to video folder")
+    parser.add_argument("--calibration_file", type=str,
+                        default="", help="path to video folder")
+    parser.add_argument("--type", type=str, default="video",
+                        help='type of "image" or "video')
+    parser.add_argument("--start", type=int, default=0,
+                        help="starting position")
+    parser.add_argument('--loop', action='store_true',
+                        help='loop video after end')
+    parser.set_defaults(loop=False)
+
+    extra_args = parser.parse_args()
+
+    # Initialize the rclpy library
+    rclpy.init(args=args)
+
+    # Create the node
+    image_publisher = ImagePublisher()
+
+    # Spin the node so the callback function is called.
+    rclpy.spin(image_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    image_publisher.destroy_node()
+
+    # Shutdown the ROS client library for Python
+    rclpy.shutdown()
+  
+if __name__ == '__main__':
+    main()
